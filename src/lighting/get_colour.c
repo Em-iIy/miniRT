3350ee96ae6058b20c95ac47fca860c5e9a9ad8e
Franakin <fddp2008@hotmail.com>
2023-01-19 16:55:24 +0100

    shadowwwwiiiiie

--- src/lighting/get_colour.c
+++ src/lighting/get_colour.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_colour.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/11 13:37:29 by fpurdom           #+#    #+#             */
-/*   Updated: 2023/01/18 18:10:56 by gwinnink         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   get_colour.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gwinnink <gwinnink@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/01/11 13:37:29 by fpurdom       #+#    #+#                 */
+/*   Updated: 2023/01/19 16:54:35 by fpurdom       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,58 @@ int	get_rgba(int r, int g, int b, int a)
 	return (r << 24 | g << 16 | b << 8 | a);
 }
 
+static t_vect3	give_rgba(int colour)
+{
+	t_vect3	colours;
+
+	colours.x = colour >> 24 & 255;
+	colours.y = colour >> 16 & 255;
+	colours.z = colour >> 8 & 255;
+	return (colours);
+}
+
 static int	get_shadow(int rgba, double intensity)
 {
-	return (get_rgba((rgba >> 24) * intensity, (rgba >> 16) * intensity, (rgba >> 8) * intensity, 255));
+	return (get_rgba((rgba >> 24 & 255) * intensity, (rgba >> 16 & 255) * intensity, (rgba >> 8 & 255) * intensity, 255));
+}
+
+static int	get_light(t_vect3 light_dir, t_vect3 pixel_dir, int colour, int shadow)
+{
+	float	d;
+	t_vect3	rgba;
+	t_vect3	ret;
+
+	d = vect3_dot_product(pixel_dir, light_dir);
+	rgba = vect3_multiply(give_rgba(colour), d);
+	ret.x = sqrt((pow(rgba.x, 2) + pow(shadow >> 24 & 255, 2)) / 2);
+	if (ret.x < (shadow >> 24 & 255))
+		ret.x = shadow >> 24 & 255;
+	ret.y = sqrt((pow(rgba.y, 2) + pow(shadow >> 16 & 255, 2)) / 2);
+	if (ret.y < (shadow >> 16 & 255))
+		ret.y = shadow >> 16 & 255;
+	ret.z = sqrt((pow(rgba.z, 2) + pow(shadow >> 8 & 255, 2)) / 2);
+	if (ret.z < (shadow >> 8 & 255))
+		ret.z = shadow >> 8 & 255;
+	//printf("<%f>\t", d);
+	//ect3_print(rgba);
+	return (get_rgba(ret.x, ret.y, ret.z, 255));
 }
 
 int	get_pixel_colour(t_vect3 ray, t_scene *scene, int colour, double t)
 {
 	t_double_intersect	sphere_intersect;
 	t_vect3				start;
+	t_vect3				normal;
 	t_object			*objs;
 	double				colides;
 	double 				test;
 	double				dist;
+	int					shadow;
 
 	objs = scene->objs;
 	start = vect3_add(scene->camera.pos, vect3_multiply(ray, t));
 	dist = vect3_abs(vect3_substract(start, scene->light.pos));
+	shadow = get_shadow(colour, scene->amlight.brightness);
 	//printf("t = %f\t", t);
 	//vect3_print(start);
 	while (objs)
@@ -54,8 +89,9 @@ int	get_pixel_colour(t_vect3 ray, t_scene *scene, int colour, double t)
 		//else if (objs->type == PLANE)
 			//colides = plane_collision(start, scene->light.pos, objs->coords, objs->orientation);
 		if (colides > 0)
-			return (get_shadow(colour, scene->amlight.brightness));
+			return (shadow);
+		normal = vect3_normalize(objs->coords, start);
 		objs = objs->next;
 	}
-	return (colour);
+	return (get_light(vect3_normalize(start, scene->light.pos), normal, colour, shadow));
 }
--- src/parser/init_scene.c
+++ src/parser/init_scene.c
@@ -6,7 +6,7 @@
 /*   By: gwinnink <gwinnink@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/11 13:40:07 by fpurdom       #+#    #+#                 */
-/*   Updated: 2023/01/18 17:01:53 by fpurdom       ########   odam.nl         */
+/*   Updated: 2023/01/19 16:53:49 by fpurdom       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 void	test_scene(t_scene *scene)
 {
 	scene->objs = NULL;
-	//obj_add_front(&scene->objs, obj_sp(obj_new(vect3(-2, 0, 2), get_rgba(255, 0, 0, 255)), 1));
-	obj_add_front(&scene->objs, obj_sp(obj_new(vect3(0, 0, 1.5), get_rgba(0, 255, 0, 255)), 1));
-	//obj_add_front(&scene->objs, obj_sp(obj_new(vect3(2, 0, 2), get_rgba(0, 0, 255, 255)), 1));
+	obj_add_front(&scene->objs, obj_sp(obj_new(vect3(-2, 0, 2), get_rgba(255, 0, 0, 255)), 1));
+	obj_add_front(&scene->objs, obj_sp(obj_new(vect3(0, 0, 2), get_rgba(0, 255, 0, 255)), 1));
+	obj_add_front(&scene->objs, obj_sp(obj_new(vect3(2, 0, 2), get_rgba(0, 0, 255, 255)), 1));
 	obj_add_front(&scene->objs, obj_pl(obj_new(vect3(0, -0.5, 0), get_rgba(120, 100, 100, 255)), vect3(0, 1, 0)));
 	scene->camera = new_camera(vect3(0, 0, -2), vect3(0, 0, 1), 110);
-	scene->light = new_light(vect3(0.5, 0, 0.3), 0.8, 0xFFFFFFFF);
-	scene->amlight = new_amblight(0.4, 0xFFFFFFFF);
+	scene->light = new_light(vect3(0, 0, 0), 0.8, 0xFFFFFFFF);
+	scene->amlight = new_amblight(0.3, 0xFFFFFFFF);
 }
 
 void	init_scene(char	*infile, t_scene *scene)
