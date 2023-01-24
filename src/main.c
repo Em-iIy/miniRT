ca0e62ff264570ec8bb52d441a811c80ba33ab24
Glenn Winnink <gwinnink@student.codam.nl>
2023-01-24 18:22:46 +0100

    clean up + removed warning

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: gwinnink <gwinnink@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/01/10 12:23:36 by gwinnink      #+#    #+#                 */
-/*   Updated: 2023/01/20 18:59:36 by fpurdom       ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/10 12:23:36 by gwinnink          #+#    #+#             */
+/*   Updated: 2023/01/24 18:18:31 by gwinnink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,37 +16,26 @@
 #include "scene.h"
 #include <stdio.h>
 #include <math.h>
-#include <stdbool.h>
 #include <stdlib.h>
-
-// #define FOV 170
-// #define DIST 1
 
 t_vect3					cast_ray(t_scene scene, double x, double y);
 
-mlx_mousefunc	func(mouse_key_t button, action_t action, modifier_key_t mods, void *void_scene)
+void	func(mouse_key_t button, action_t action, modifier_key_t mods, void *void_scene)
 {
 	t_scene	*scene;
 	int32_t	x, y;
 	t_vect3	ray;
-	// const double	step = DIST * tan((FOV / 2) * (M_PI / 180)) / WIDTH / 2;
-	// const double	dist = (WIDTH / 2) / tan(((FOV / 2) * (M_PI / 180)) * (M_PI / 180));
 
 	scene = (t_scene *)void_scene;
 	mlx_get_mouse_pos(scene->mlx, &x, &y);
 	printf("click (%d, %d)\n", x, y);
-	// ray = vect3((x - WIDTH / 2) * step, (y - HEIGHT / 2) * step, DIST);
-	// vect3_print(vect3_normalize(vect3(0, 0, 0), ray));
 	ray = cast_ray(*scene, x, y - HEIGHT / 2);
-	//vect3_print(ray);
 	get_collision(void_scene, ray);
 }
 
 int	main(void)
 {
 	t_scene			scene;
-	// const double	dist = (WIDTH / 2) / tan(((FOV / 2) * (M_PI / 180)) * (M_PI / 180));
-	// const double	step = DIST * tan((FOV / 2) * (M_PI / 180)) / WIDTH / 2;
 	t_vect3			ray;
 	int				color;
 	mlx_image_t		*img;
@@ -59,8 +48,6 @@ int	main(void)
 	{
 		for (double x = 0; x < WIDTH; x++)
 		{
-			// ray = vect3_add(vect3((x - WIDTH / 2), (y - HEIGHT / 2), dist), scene.camera.pos);
-			// ray = vect3((x - WIDTH / 2) * step, (y - HEIGHT / 2) * step, DIST);
 			ray = cast_ray(scene, x, HEIGHT / 2 - y);
 			color = get_collision((void *)&scene, ray);
 			mlx_put_pixel(img, x, y, color);
