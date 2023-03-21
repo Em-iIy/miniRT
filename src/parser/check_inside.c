c03457dc1da887d3916dd7a08b3130b44467bb3b
Franakin <fddp2008@hotmail.com>
2023-03-21 16:14:51 +0100

    light glitch fix

--- src/parser/check_inside.c
+++ src/parser/check_inside.c
@@ -1,17 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   check_inside.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gwinnink <gwinnink@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/09 14:24:08 by gwinnink          #+#    #+#             */
-/*   Updated: 2023/03/21 12:49:18 by gwinnink         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   check_inside.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gwinnink <gwinnink@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/09 14:24:08 by gwinnink      #+#    #+#                 */
+/*   Updated: 2023/03/21 16:06:14 by fpurdom       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "objects.h"
 #include "error.h"
+#include <math.h>
 
 static void	inside_sphere(t_object *sphere, t_vect3 cam_pos, t_vect3 light_pos)
 {
@@ -32,13 +33,31 @@ static void	inside_plane(t_object *plane, t_vect3 cam_pos, t_vect3 light_pos)
 static void	inside_cyl(t_object *cyl, t_vect3 cam_pos, t_vect3 light_pos)
 {
 	const t_vect3	d = cyl->orient * cyl->lenght;
-	const t_vect3	pd = cam_pos - cyl->pos;
-	const double	dot = vect3_dot(pd, d);
+	t_vect3			pd;
+	double			dot;
 
-	(void)light_pos;
+	pd = light_pos - cyl->pos;
+	dot = vect3_dot(pd, d);
+	if (vect3_dot(pd, pd) - dot * dot
+		/ (cyl->lenght * cyl->lenght) == cyl->radius * cyl->radius
+		&& vect3_dot(light_pos - cyl->pos, cyl->orient) > 0
+		&& vect3_dot(light_pos - (cyl->pos + cyl->orient * cyl->lenght),
+			-cyl->orient) > 0)
+		error_msg_exit("Light source exactly on cylinder\n", 1);
+	pd = cam_pos - cyl->pos;
+	dot = vect3_dot(pd, d);
 	if (vect3_dot(pd, pd) - dot * dot
 		/ (cyl->lenght * cyl->lenght) < cyl->radius * cyl->radius)
 		cyl->inside = -1;
+}
+
+static void	inside_circle(t_object *c, t_vect3 cam_pos, t_vect3 light_pos)
+{
+	if (sqrt(pow(light_pos[0] - c->pos[0], 2) + pow(light_pos[1] - c->pos[1], 2)
+			+ pow(light_pos[2] - c->pos[2], 2)) <= c->radius)
+		error_msg_exit("Light source exactly on cylinder\n", 1);
+	if (vect3_dot(cam_pos - c->pos, c->orient) < 0)
+		c->inside = -1;
 }
 
 void	check_inside(t_object *objs, t_vect3 cam_pos, t_vect3 light_pos)
@@ -47,7 +66,9 @@ void	check_inside(t_object *objs, t_vect3 cam_pos, t_vect3 light_pos)
 	{
 		if (objs->type == SPHERE)
 			inside_sphere(objs, cam_pos, light_pos);
-		else if (objs->type == PLANE || objs->type == CIRCLE)
+		else if (objs->type == CIRCLE)
+			inside_circle(objs, cam_pos, light_pos);
+		else if (objs->type == PLANE)
 			inside_plane(objs, cam_pos, light_pos);
 		else if (objs->type == CYLINDER)
 			inside_cyl(objs, cam_pos, light_pos);
